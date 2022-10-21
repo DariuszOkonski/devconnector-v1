c0f1e935bbb089edf66b149b06f8cfb0f04e1cee
Dariusz Okoński <darek200180@gmail.com>
2022-10-21 15:17:43 +0200

    add route for adding experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -151,7 +151,6 @@ router.get('/user/:user_id', async (req, res) => {
  * @desc    Delete profile, user & posts
  * @access  Private
  */
-
 router.delete('/', auth, async (req, res) => {
   try {
     /**
@@ -168,5 +167,52 @@ router.delete('/', auth, async (req, res) => {
     return res.status(500).send('Server Error')
   }
 })
+
+/**
+ * @route   PUT api/profile/experience
+ * @desc    Add profile experience
+ * @access  Private
+ */
+router.put(
+  '/experience',
+  [
+    auth,
+    [
+      check('title', 'Title is required').not().isEmpty(),
+      check('company', 'Company is required').not().isEmpty(),
+      check('from', 'From date is required').not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() })
+    }
+
+    const {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description,
+    } = req.body
+
+    const newExp = { title, company, location, from, to, current, description }
+
+    try {
+      const profile = await Profile.findOne({ user: req.user.id })
+
+      profile.experiencs.unshift(newExp)
+      await profile.save()
+
+      return res.json(profile)
+    } catch (err) {
+      console.error(err.message)
+      return res.status(500).send('Server Error')
+    }
+  },
+)
 
 module.exports = router
