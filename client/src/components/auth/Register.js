38f976c2850de311ec032c52d07b15ff835059c4
Dariusz Okoński <darek200180@gmail.com>
2022-10-25 16:00:32 +0200

    working on register

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,4 +1,5 @@
 import React, { Fragment, useState } from 'react'
+import axios from 'axios'
 
 function Register() {
   const [formData, setFormData] = useState({
@@ -14,15 +15,41 @@ function Register() {
     setFormData({ ...formData, [e.target.name]: e.target.value })
   }
 
-  const onSubmit = (e) => {
+  const onSubmit = async (e) => {
     e.preventDefault()
 
     if (password !== password2) {
       console.log('Password do not match')
-      return
+    } else {
+      console.log('success')
+      //   const newUser = {
+      //     name,
+      //     email,
+      //     password,
+      //   }
+
+      //   try {
+      //     const config = {
+      //       headers: {
+      //         'Content-Type': 'application/json',
+      //       },
+      //     }
+      //     const body = JSON.stringify(newUser)
+
+      //     const res = await axios.post(
+      //       'http://localhost:5000/api/users',
+      //       body,
+      //       config,
+      //     )
+
+      //     // console.log(res.data)
+      //     console.log(res)
+      //   } catch (err) {
+      //     console.error(err.respose.data)
+      //   }
     }
 
-    console.log(formData)
+    // console.log(formData)
 
     // setFormData({
     //   name: '',
