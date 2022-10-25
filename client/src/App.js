8a0000f9e65a2609805626a63300fd5d76d84bdd
Dariusz Okoński <darek200180@gmail.com>
2022-10-25 14:53:12 +0200

    add routes to all links

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,27 @@
 import { Fragment } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
+import Login from './components/auth/Login'
+import Register from './components/auth/Register'
 import Landing from './components/Landing'
 import Navbar from './components/Navbar'
 
 const App = () => {
   return (
-    <Fragment>
-      <Navbar />
-      <Landing />
-    </Fragment>
+    <BrowserRouter>
+      <Fragment>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Landing />} />
+        </Routes>
+        <section className="container">
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </section>
+      </Fragment>
+    </BrowserRouter>
   )
 }
 
