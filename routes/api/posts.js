8b30b23abc0e38e0c1c9b95bb5719ec6347152aa
Dariusz Okoński <darek200180@gmail.com>
2022-10-25 13:20:20 +0200

    add route for add, remove comment

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -193,7 +193,6 @@ router.put('/unlike/:id', auth, async (req, res) => {
  * @desc    Comment on a post
  * @access  Private
  */
-
 router.post(
   '/comment/:id',
   [auth, check('text', 'Text is required').not().isEmpty()],
@@ -234,5 +233,52 @@ router.post(
     }
   },
 )
+
+/**
+ * @route   DELETE api/posts/comment/:id/:comment_id
+ * @desc    Delete comment
+ * @access  Private
+ */
+
+router.delete('/comment/:id/:comment_id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id)
+    if (!post) {
+      return res.status(404).send({ msg: 'Post not found' })
+    }
+
+    // pull out comment
+    const comment = post.comments.find(
+      (comment) => comment.id === req.params.comment_id,
+    )
+
+    // make sure comment exists
+    if (!comment) {
+      return res.status(404).json({ msg: 'Comment does not exist' })
+    }
+
+    // check user
+    if (comment.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'User not authorized' })
+    }
+
+    // get remove index
+    const removeIndex = post.comments
+      .map((comment) => comment.user.toString())
+      .indexOf(req.user.id)
+
+    post.comments.splice(removeIndex, 1)
+
+    await post.save()
+
+    return res.json(post.comments)
+  } catch (err) {
+    console.error(err.message)
+    if (err.kind === 'ObjectId') {
+      return res.status(404).send({ msg: 'Element not found' })
+    }
+    return res.status(500).send('server error')
+  }
+})
 
 module.exports = router
