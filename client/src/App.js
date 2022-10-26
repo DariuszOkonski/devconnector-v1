f8dca2769c8d6af613a9a23cecc1107a30cc5dfc
Dariusz Okoński <darek200180@gmail.com>
2022-10-26 14:28:25 +0200

    add basic redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,33 @@
-import { Fragment } from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import './App.css'
-import Login from './components/auth/Login'
-import Register from './components/auth/Register'
-import Landing from './components/Landing'
-import Navbar from './components/Navbar'
+import { Fragment } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
+import Landing from "./components/Landing";
+import Navbar from "./components/Navbar";
+// Redux
+import { Provider } from "react-redux";
+import store from "./store";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Fragment>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Landing />} />
-        </Routes>
-        <section className="container">
+    <Provider store={store}>
+      <BrowserRouter>
+        <Fragment>
+          <Navbar />
           <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
+            <Route path="/" element={<Landing />} />
           </Routes>
-        </section>
-      </Fragment>
-    </BrowserRouter>
-  )
-}
+          <section className="container">
+            <Routes>
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+            </Routes>
+          </section>
+        </Fragment>
+      </BrowserRouter>
+    </Provider>
+  );
+};
 
-export default App
+export default App;
