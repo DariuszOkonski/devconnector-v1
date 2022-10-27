da20167b18d64b1e7888325d0447244260574396
Dariusz Okoński <darek200180@gmail.com>
2022-10-27 14:31:57 +0200

    add redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
+import "./App.css";
 import { Fragment } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./App.css";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 import Landing from "./components/Landing";
